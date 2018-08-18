f00927169bf116adf1e673c2da6d50e0a3254309
tryking <tryking@foxmail.com>
2018-08-18 17:11:23 +0800

    更新状态

--- porn/porn/spiders/sis_spider.py
+++ porn/porn/spiders/sis_spider.py
@@ -8,7 +8,30 @@ import scrapy
 from ..libs.common import *
 
 """
-
+2018-08-18 17:09:17 [scrapy.statscollectors] INFO: Dumping Scrapy stats:
+{'downloader/exception_count': 1,
+ 'downloader/exception_type_count/twisted.internet.error.TimeoutError': 1,
+ 'downloader/request_bytes': 112441,
+ 'downloader/request_count': 281,
+ 'downloader/request_method_count/GET': 281,
+ 'downloader/response_bytes': 5182726,
+ 'downloader/response_count': 280,
+ 'downloader/response_status_count/200': 280,
+ 'finish_reason': 'finished',
+ 'finish_time': datetime.datetime(2018, 8, 18, 9, 9, 17, 978818),
+ 'log_count/DEBUG': 327,
+ 'log_count/INFO': 13,
+ 'memusage/max': 165904384,
+ 'memusage/startup': 95772672,
+ 'request_depth_max': 2,
+ 'response_received_count': 280,
+ 'retry/count': 1,
+ 'retry/reason_count/twisted.internet.error.TimeoutError': 1,
+ 'scheduler/dequeued': 281,
+ 'scheduler/dequeued/disk': 281,
+ 'scheduler/enqueued': 281,
+ 'scheduler/enqueued/disk': 281,
+ 'start_time': datetime.datetime(2018, 8, 18, 9, 4, 4, 883440)}
 """
 
 SAVE_DIR = 'texts' + os.path.sep + 'sis'
