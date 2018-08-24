23d6c2b708e23840a3db8239aeae07cb6b10c3bb
dengkaiting <dengkaiting@migu.cn>
2018-08-24 19:36:27 +0800

    更新索引

--- news/news/libs/索引.py
+++ news/news/libs/索引.py
@@ -13,3 +13,9 @@ db.getCollection('sogo_wechat_article').createIndex({'wechat_id':1,'article_titl
 # 一个来源一个文章唯一，插入前查找时速度快
 db.getCollection('jinritoutiao_article').createIndex({'source':1,'article_title': 1},{unique:true})
 """
+
+# news_kr30
+"""
+# 一个作者一个文章唯一，插入前查找时速度快
+db.getCollection('news_kr30').createIndex({'article_author':1,'article_title': 1},{unique:true})
+"""
